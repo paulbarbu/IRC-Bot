3ab56c9276db1e7881a4ff75084c889e67f47100
PauLLiK <paullik@yahoo.com>
2011-06-25 20:37:23 +0300

    config file

---
+++ src/config.py
@@ -0,0 +1,19 @@
+#internals
+owner = [
+        'paullik',
+        'foobarfoo',
+        ]
+search = "http://google.ro"
+
+#connection and authentication
+server = 'chat.freenode.net'
+port = 6667
+
+nick = 'PPyBot'
+realName = 'Paul Python Bot'
+
+channel = '#botwar'
+
+#messages
+quit_msg = 'Bye bye'
+
