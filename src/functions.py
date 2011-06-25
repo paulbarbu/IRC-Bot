970b61b487934c4dc66f0065ad3373cbc3d8eae9
PauLLiK <paullik@yahoo.com>
2011-06-25 20:40:17 +0300

    proper __doc__

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 def get_sender(msg):
     """Returns the user(string) that sent the message
+
     Searches the string to find the user that sent it
     """
     return msg.split(":")[1].split('!')[0]
