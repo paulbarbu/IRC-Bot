a0fe2c7dd08537e8429f61024a536a20cf05ac2e
PauLLiK <paullik@yahoo.com>
2011-06-25 22:50:23 +0300

    log writing

--- src/ircbot.py
+++ src/ircbot.py
@@ -10,21 +10,36 @@ channel_join = 'JOIN ' + channel + '\r\n'
 channel_part = 'PART ' + channel + ' :' + quit_msg + '\r\n'
 
 privmsg = 'PRIVMSG ' + channel + ' :'
+quit = 'QUIT\r\n'
 #}
+
+dt = get_datetime()
+
+logfile = log + dt['date']
+content = 'Started on {0}:{1}, channel: {2}, with nick: {3}'.format(server,
+        port, channel, nick)
+
+log_write(logfile, dt['time'], ' <> ', content + '\n')
 
 try:
     irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.socket:
-    print 'Could not create a socket!'
+    content = 'Could not create a socket!'
+    log_write(logfile, dt['time'], ' <> ', content + '\n')
+    print content
 else:
     try:
         irc.connect((server, port))
-        response = irc.recv(4096)
+        receive = irc.recv(4096) #TODO debug with this line commented and remove buff = ""
     except IOError:
-        print 'Could not connect to {0}{1}'.format(server, port)
+        content = 'Could not connect to {0}{1}'.format(server, port)
+        log_write(logfile, dt['time'], ' <> ', content + '\n')
+        print content
     else:
         buff = ""
-        print 'Connected to {0}:{1}'.format(server, port)
+        content = 'Connected to {0}:{1}'.format(server, port)
+        log_write(logfile, dt['time'], ' <> ', content + '\n')
+        print content
 
         #Join server & authenticate
         irc.send(nick_auth)
@@ -32,52 +47,79 @@ else:
 
         #Join channel
         irc.send(channel_join)
-        print 'Joined: {0}'.format(channel)
+        content = 'Joined: {0}'.format(channel)
+        log_write(logfile, dt['time'], ' <> ', content + '\n')
+        print content
 
         while True:
-            response = irc.recv(4096)
-            buff = buff + response
+            receive = irc.recv(4096)
+            buff = buff + receive
 
             if -1 != buff.find('\n'):
                 command = buff[0 : buff.find('\n')]
                 buff = buff[buff.find('\n')+1:]
 
                 if -1 != command.find('PING'): #PING PONG between server and client
-                    irc.send('PONG ' + command.split()[1] + '\r\n')
+                    response = 'PONG ' + command.split()[1] + '\r\n'
+                    irc.send(response)
+
+                    dt = get_datetime()
+                    log_write(logfile, dt['time'], ' <> ', command + '\n')
+                    log_write(logfile, dt['time'], ' <> ', response)
 
                 elif -1 != command.find('!search'): # !google <nick>
-                    command = command.split()
+                    nick = command.split()
 
-                    if 4 == len(command): #no nick given
-                        irc.send(privmsg + 'Usage: !search <nick>\r\n')
+                    if 4 == len(nick): #no nick given
+                        response = privmsg + 'Usage: !search <nick>\r\n'
                     else:
-                        irc.send(privmsg + str(command[-1]) +
-                            ', please search on: ' + search + '\r\n')
+                        response = privmsg + str(nick[-1]) + ', please search on: ' + search + '\r\n'
+
+                    irc.send(response)
+
+                    dt = get_datetime()
+                    log_write(logfile, dt['time'], ' <> ', command + '\n')
+                    log_write(logfile, dt['time'], ' <> ', response)
 
                 elif -1 != command.find('!wiki'): # !wiki <search term>
-                    command = command.split('!wiki ')
-                    if 1 == len(command): #no search term given
-                        irc.send(privmsg + 'Usage: !wiki <search term>\r\n')
+                    wlink = command.split('!wiki ')
+                    if 1 == len(wlink): #no search term given
+                        response = privmsg + 'Usage: !wiki <search term>\r\n'
                     else:
-                        irc.send(privmsg + 'http://en.wikipedia.org/wiki/' +
-                            command[1].lstrip().replace(' ', '_')
-                            + '\r\n')
+                        response = privmsg + 'http://en.wikipedia.org/wiki/' + wlink[1].lstrip().replace(' ', '_') + '\r\n'
+
+                    irc.send(response)
+
+                    dt = get_datetime()
+                    log_write(logfile, dt['time'], ' <> ', command + '\n')
+                    log_write(logfile, dt['time'], ' <> ', response)
 
                 elif -1 != command.find('!quit'): # !quit
+                    dt = get_datetime()
                     sender = get_sender(command)
                     if sender in owner:
-                        irc.send(channel_part)
+                        response = channel_part
+                        irc.send(response)
+                        log_write(logfile, dt['time'], ' <> ', command + '\n')
+                        log_write(logfile, dt['time'], ' <> ', response)
                         break;
                     else:
-                        irc.send(privmsg + 'This command can be run only by the'
-                                + ' owner(s)!\r\n')
+                        response = privmsg + 'This command can be run only by the' + ' owner(s)!\r\n'
+                        irc.send(response)
+                        log_write(logfile, dt['time'], ' <> ', command + '\n')
+                        log_write(logfile, dt['time'], ' <> ', response)
 
                 else:
                     buff = ""
 
         #Leave(part) channel
-        irc.send('QUIT\r\n')
+        irc.send(quit)
+        dt = get_datetime()
+        log_write(logfile, dt['time'], ' <> ', quit)
 
         irc.close()
 
-        print "Exited with message: {0}".format(quit_msg)
+        content = 'Exited with message: {0}'.format(quit_msg)
+        log_write(logfile, dt['time'], ' <> ', content + '\n')
+        print content
+
