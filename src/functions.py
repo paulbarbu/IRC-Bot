1d4c4c65436f69e25d9bb06f870fbeb0fd4b03e0
PauLLiK <paullik@yahoo.com>
2011-06-25 20:37:38 +0300

    functions file

---
+++ src/functions.py
@@ -0,0 +1,5 @@
+def get_sender(msg):
+    """Returns the user(string) that sent the message
+    Searches the string to find the user that sent it
+    """
+    return msg.split(":")[1].split('!')[0]
