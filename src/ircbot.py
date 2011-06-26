0a3305a6f48e7c2172f3e60842573fde6bd76e62
PauLLiK <paullik@yahoo.com>
2011-06-26 19:04:28 +0300

    fully implemented modules

--- src/ircbot.py
+++ src/ircbot.py
@@ -6,28 +6,28 @@ import re
 
 #these modules must exist in cwd
 try:
-    from config import *
+    import config
     from functions import *
     import err
 except ImportError:
-    sys.exit('Could not load all modules!')
+    sys.exit(err.load_module)
 
 
-cfg = check_cfg(owner, search, server, nick, realName, channel,
-        log, c_lst, quit)
+cfg = check_cfg(config.owner, config.search, config.server, config.nick,
+        config.realName, config.channel, config.log, config.cmds_list, config.quit)
 
 if not cfg:
     sys.exit('Config error!')
 
-if not check_channel(channel):
+if not check_channel(config.channel):
     sys.exit('Invalid channel!')
 
 #name the log file to write into
 dt = get_datetime()
-logfile = log + dt['date'] + '.log'
+logfile = config.log + dt['date'] + '.log'
 
-content = 'Started on {0}:{1}, channel: {2}, with nick: {3}'.format(server,
-        port, channel, nick)
+content = 'Started on {0}:{1}, channel: {2}, with nick: {3}'.format(config.server,
+        config.port, config.channel, config.nick)
 
 try:
     log_write(logfile, dt['time'], ' <> ', content + '\n')
@@ -47,10 +47,10 @@ except socket.socket:
     print content
 else:
     try:
-        irc.connect((server, port))
+        irc.connect((config.server, config.port))
         receive = irc.recv(4096) #TODO debug with this line commented and remove buff = ""
     except IOError:
-        content = 'Could not connect to {0}:{1}'.format(server, port)
+        content = 'Could not connect to {0}:{1}'.format(config.server, config.port)
 
         try:
             log_write(logfile, dt['time'], ' <> ', content + '\n')
@@ -62,7 +62,7 @@ else:
     else:
         buff = ""
 
-        content = 'Connected to {0}:{1}'.format(server, port)
+        content = 'Connected to {0}:{1}'.format(config.server, config.port)
         try:
             log_write(logfile, dt['time'], ' <> ', content + '\n')
         except IOError:
@@ -71,12 +71,12 @@ else:
         print content
 
         #Join server & authenticate
-        irc.send(nick_auth)
-        irc.send(user_auth)
+        irc.send(config.nick_auth)
+        irc.send(config.user_auth)
 
         #Join channel
-        irc.send(channel_join)
-        content = 'Joined: {0}'.format(channel)
+        irc.send(config.channel_join)
+        content = 'Joined: {0}'.format(config.channel)
 
         try:
             log_write(logfile, dt['time'], ' <> ', content + '\n')
@@ -85,7 +85,7 @@ else:
 
         print content
 
-        while alive:
+        while config.alive:
             receive = irc.recv(4096)
             buff = buff + receive
             response = ""
@@ -101,17 +101,23 @@ else:
                     #search in commands list only if the message from server
                     #contains two '!'(exclamation marks) in it
                     #one from command, the other from the user's nick
-                    for k in c_lst:
+                    for k in config.cmds_list:
                         if -1 != command.find('!' + k):
-                            print k
                             try:
                                 mod = 'cmds.' + k
                                 mod = __import__(mod, globals(), locals(), [k])
-                                mod.hello()
-                            except ImportError:
-                                response = privmsg + 'Command "{0}" does not exists although it\'s specified in commands list!\r\n'.format(k)
+                            except ImportError: #inexistent module
+                                response = config.privmsg + err.c_inexistent.format(k)
+                            else:
 
-                            break
+                                try:
+                                    get_response = getattr(mod, k)
+                                except AttributeError: #function not defined in module
+                                    response = config.privmsg + err.c_invalid.format(k)
+                                else:
+
+                                    response = get_response(command)
+                                    break
 
                 #send the response and log it
                 if len(response):
@@ -125,57 +131,19 @@ else:
                         print err.log_failure
 
                 buff = ""
-
-                #elif -1 != command.find('!search'): # !google <nick>
-                    #nick = command.split()
-
-                    #if 4 == len(nick): #no nick given
-                        #response = privmsg + 'Usage: !search <nick>\r\n'
-                    #else:
-                        #response = privmsg + str(nick[-1]) + ', please search on: ' + search + '\r\n'
-
-                #elif -1 != command.find('!wiki'): # !wiki <search term>
-                    #wlink = command.split('!wiki ') #notice the trailing space
-                    #if 1 == len(wlink): #no search term given
-                        #response = privmsg + 'Usage: !wiki <search term>\r\n'
-                    #else:
-                        #response = privmsg + 'http://en.wikipedia.org/wiki/' + wlink[1].lstrip().replace(' ', '_') + '\r\n'
-
-                #elif -1 != command.find('!quit'): # !quit -> PART #channel
-                    #sender = get_sender(command)
-                    #if sender in owner:
-                        #response = channel_part
-                        #alive = False
-                    #else:
-                        #response = privmsg + 'This command can be run only by the' + ' owner(s)!\r\n'
-
-                #elif -1 != command.find('!answer') or -1 != command.find('!42'): # !answer
-                    #response = privmsg + 'The Answer to the Ultimate Question of Life, the Universe, and Everything is 42!\r\n'
-
-                #else:
-                    #buff = ""
-
-                #if len(response):
-                    #irc.send(response)
-
-                    #try:
-                        #dt = get_datetime()
-                        #log_write(logfile, dt['time'], ' <> ', command + '\n')
-                        #log_write(logfile, dt['time'], ' <> ', response)
-                    #except IOError:
-                        #print err.log_failure
+        #}while config.alive
 
         #Quit server
-        irc.send(quit)
+        irc.send(config.quit)
         dt = get_datetime()
         try:
-            log_write(logfile, dt['time'], ' <> ', quit)
+            log_write(logfile, dt['time'], ' <> ', config.quit)
         except IOError:
             print err.log_failure
 
         irc.close()
 
-        content = 'Exited with message: {0}'.format(quit_msg)
+        content = 'Exited with message: {0}'.format(config.quit_msg)
         try:
             log_write(logfile, dt['time'], ' <> ', content + '\n')
         except IOError:
