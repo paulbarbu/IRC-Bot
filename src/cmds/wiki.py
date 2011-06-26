286f5947a7715551565310537d79925477aeb620
PauLLiK <paullik@yahoo.com>
2011-06-26 23:57:34 +0300

    !wiki returns paragraph using BeautifulSoup

--- src/cmds/wiki.py
+++ src/cmds/wiki.py
@@ -1,4 +1,6 @@
 try:
+    from BeautifulSoup import BeautifulSoup
+    import urllib2
     import config
     import err
 except ImportError:
@@ -7,12 +9,32 @@ except ImportError:
 def wiki(command):
     """Replies a wiki link with the searched term
 
-    Tries to return a link to the wiki page for the <search term>
+    Tries to return a link to the wiki page for the <search term> and the first
+    paragraph of the page
     """
     wlink = command.split('!wiki ') #notice the trailing space
     if 1 == len(wlink): #no search term given
-        response = config.privmsg + 'Usage: !wiki <search term>\r\n'
+        response = 'Usage: !wiki <search term>\r\n'
     else:
-        response = config.privmsg + 'http://en.wikipedia.org/wiki/' + wlink[1].lstrip().replace(' ', '_') + '\r\n'
+        response = 'http://en.wikipedia.org/wiki/' + wlink[1].lstrip().replace(' ', '_')
+        response = response + '\r\n' + config.privmsg + get_para(response) + '\r\n'
 
-    return response
+    return config.privmsg + response
+
+def get_para(wlink):
+    """Gets the first paragraph from a wiki link
+
+    """
+    msg = ''
+    try:
+        page_request = urllib2.Request(wlink)
+        page_request.add_header('User-agent', 'Mozilla/5.0')
+        page = urllib2.urlopen(page_request)
+    except IOError:
+        msg = 'Cannot acces link!'
+    else:
+
+        soup = BeautifulSoup(page)
+        msg = ''.join(soup.find('div', { 'id' : 'bodyContent'}).p.findAll(text=True))
+
+    return msg
