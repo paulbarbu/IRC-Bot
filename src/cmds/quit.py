38d38bf91078c3dde8a02a8325bf8277c19fc425
PauLLiK <paullik@yahoo.com>
2011-06-26 18:36:08 +0300

    finished quit module

--- src/cmds/quit.py
+++ src/cmds/quit.py
@@ -1,2 +1,15 @@
-def hello():
-    print"HELLO QUIT WORLD"
+try:
+    import config
+    from functions import *
+except ImportError:
+    sys.exit('Could not load all modules!')
+
+def quit(command):# !quit -> PART #channel
+    sender = get_sender(command)
+    if sender in config.owner:
+        response = config.channel_part
+        config.alive = False
+    else:
+        response = config.privmsg + 'This command can be run only by the owner(s)!\r\n'
+
+    return response
