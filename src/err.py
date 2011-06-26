100b80d049d0039c209c0999784e3bec09e9f939
PauLLiK <paullik@yahoo.com>
2011-06-26 19:05:05 +0300

    loading modules errors

--- src/err.py
+++ src/err.py
@@ -1,2 +1,5 @@
 #errors
-log_failure = 'could not write to logs!'
+log_failure = 'Could not write to logs!'
+c_inexistent = 'Command "{0}" unavailable!\r\n'
+c_invalid = 'Command "{0}" has not a valid function to call!\r\n'
+load_module = 'Could not load a module!'
