3043e3be167befe544bf6fd6799006ab90d677d7
PauLLiK <paullik@yahoo.com>
2011-07-04 19:17:41 +0300

    added number of commands to the message

--- src/cmds/help.py
+++ src/cmds/help.py
@@ -8,7 +8,8 @@ def help(command): # !help
     """Returns a string containing all the available commands
 
     """
-    response = 'Available commands: '
+    response = str(len(config.cmds_list)) + ' available commands: '
+
     for i in config.cmds_list:
         response = response + '!' + i + ' '
     return response
