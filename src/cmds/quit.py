6094c1a3633b00ecb67373518cd46002d79d5366
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2012-04-21 13:03:18 +0300

    checking if the user is not only a owner but also registered

--- src/cmds/quit.py
+++ src/cmds/quit.py
@@ -17,32 +17,35 @@ def quit(components): # !quit [chan_name]+ -> PART #channel
     """
     response = ''
     leave = []
+    registered = is_registered(components['sender'])
 
-    if components['sender'] in config.owner: #only the owner(s) can run this command
-        response = []
-        response.append('PART')
-
-        quit_command = components['arguments'].split('!quit ')
-
-        if 2 == len(quit_command): #argument(s) supplied
-            arg_channels = quit_command[1].lstrip().split(' ')
-
-            for chan in arg_channels:
-                chan = chan.strip('\r')
-                if chan in config.channels: #valid channel
-                    leave.append(chan)
-                    config.channels.remove(chan)
-
-            if len(leave):
-                response.append(','.join(leave))
-            else:
-                response = 'Invalid channel name(s)!'
-
-        else: #no arguments supplied, quitting all channels
-            response.append(','.join(config.channels))
-            config.channels = []
-
+    if registered is None:
+        response = 'An unexpected error occurred while fetching data!'
     else:
-        response = 'This command can be run only by the owner(s)!'
+        if components['sender'] in config.owner and registered: #only the owner(s) can run this command
+            response = []
+            response.append('PART')
+
+            quit_command = components['arguments'].split('!quit ')
+
+            if 2 == len(quit_command): #argument(s) supplied
+                arg_channels = quit_command[1].lstrip().split(' ')
+
+                for chan in arg_channels:
+                    chan = chan.strip('\r')
+                    if chan in config.channels: #valid channel
+                        leave.append(chan)
+                        config.channels.remove(chan)
+
+                if len(leave):
+                    response.append(','.join(leave))
+                else:
+                    response = 'Invalid channel name(s)!'
+
+            else: #no arguments supplied, quitting all channels
+                response.append(','.join(config.channels))
+                config.channels = []
+        else:
+            response = 'This command can be run only by the authenticated owners!'
 
     return response
