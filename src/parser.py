6f791e740e267735fe5696cf3f6c53e6ba7c5a60
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2012-04-29 23:13:03 +0300

    proper parsing of the irc protocol received by a client and, of course, components renaming to actually match semantics

--- src/parser.py
+++ src/parser.py
@@ -5,30 +5,30 @@ def parse_command(command):
 
     A dictionary will be filled by the data of the command, the command is as
     follows:
-    :sender ACTION [optional_args] :arguments
+    :sender ACTION action_args :arguments
 
-    sender(string) is the user who sent the command
+    sender(string) is the user who sent the command (only the user's nick)
 
-    ACTION(string) can be one of the following: PING, KICK, PRIVMSG, QUIT, JOIN, etc.
-    For more info on this check: http://www.irchelp.org/irchelp/rfc/chapter4.html#c4_2
+    action(string) can be one of the following: PING, KICK, PRIVMSG, QUIT, etc.
+    Check: http://www.irchelp.org/irchelp/rfc/chapter4.html#c4_2
 
-    optional_args(list of strings) depends on the ACTION, they are usually the
+    action_args(list of strings) depends on the ACTION, they are usually the
     channel or the user whom is the command for(see KICK, PRIVMSG, etc.), this
     will be a list and the items in the list will be the words that form the
-    optional arguments
+    actual arguments
 
     arguments(string) depends on the ACTION
 
-    eg: ':foo KICK #chan user :reason'
-        sender: ':foo'
-        ACTION: 'KICK'
-        optional_args: ['#chan', 'user']
+    eg: the command ':foo!foo@domain.tld KICK #chan user :reason' will become:
+        sender: 'foo'
+        action: 'KICK'
+        action_args: ['#chan', 'user']
         arguments: 'reason'
     '''
     components = {
             'sender' : '',
             'action' : '',
-            'optional_args' : [],
+            'action_args' : [],
             'arguments' : '',
 
     }
@@ -44,20 +44,22 @@ def parse_command(command):
 
         command = command[space_pos + 1:]
 
-        if ':' != command[0]: # optional_args are present
+        if ':' != command[0]: # action_args are present
             colon_pos = command.find(':')
-            components['optional_args'] = command[:colon_pos-1].split()
-            command = command[colon_pos + 1:]
 
-        if ':' == command[0]: # no optional_args
+            if -1 == colon_pos:
+                colon_pos = len(command)+1
+
+            components['action_args'] = command[:colon_pos-1].split()
+            command = command[colon_pos:]
+
+        if command and ':' == command[0]: # arguments are present
             components['arguments'] = command[1:]
-        else: # optional_args were present
-            components['arguments'] = command
 
     else: # the server sent a command
         space_pos = command.find(' ')
         components['action'] = command[:space_pos]
-        components['arguments'] = command[space_pos+1:]
+        components['arguments'] = command[space_pos+2:]
 
     components['arguments'] = components['arguments'].rstrip('\r')
 
