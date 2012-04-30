402f19a54bff31757c42f1958b64f27a6b91a9fa
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2012-05-01 01:52:56 +0300

    further addtion to TODOs

--- src/ircbot.py
+++ src/ircbot.py
@@ -9,8 +9,12 @@ from functions import *
 
 #TODO: change is_registered to use the newly created functions (same as run)
 #TODO: change is_registered to use the exactly the same socket as the "main" one
+
 #TODO: join and quit can send themselves the commands using the "main" socket
 #TODO: if doing the above then a message is possible on the channel
+#TODO: if the "main" socket is not reliable (pass it to all cmds or doens't work
+#TODO: I can try to send the message manually near the JOIN or PART
+
 #TODO: test the __name__ part of this file
 
 def run(socket, channels, cmds, nick, logfile):
